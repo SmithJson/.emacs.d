4c1eaf348867a9b66ae95f2fb498d04007e56641
zhangl <zhangl@mail.ncut.edu.cn>
2020-10-14 19:50:53 +0800

    20/10/14 Update Emacs config

--- init.el
+++ init.el
@@ -4,15 +4,14 @@
 
 ;; 打开配置文件
 (defun open-my-init-file()
-	(interactive)
-	(find-file "~/.emacs.d/init.el"))
+  (interactive)
+  (find-file "~/.emacs.d/init.el"))
 
 (require 'init-packages)
 (require 'init-ui)
 (require 'init-better-defaults)
-(require 'init-org)
 (require 'init-keybingings)
+(require 'init-org)
 
 (setq custom-file (expand-file-name "lisp/custom.el" user-emacs-directory))
-
 (load-file custom-file)
